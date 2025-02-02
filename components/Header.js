49dbc4eb71be2eee3afda0938aceb1721dd8e089
Alexander Hu <alexanderhu77@ucla.edu>
2025-02-01 19:02:59 -0800

    updated links and about page

--- components/Header.js
+++ components/Header.js
@@ -54,6 +54,7 @@ class Header extends HTMLElement {
               <li><a href="contact.html">Contact</a></li>
               <li><a href="posterdayarchive.html">Poster Day Archive</a></li>
               <li><a href="recent.html">Recent Events</a></li>
+              <!-- <li><a href="future_events.html">Future Events</a></li> -->
               <li><a href="https://forms.gle/3kmusUSjUaAQ7LQo6" target="_blank">Stay Updated!</a></li>
               </ul>
           </nav>
