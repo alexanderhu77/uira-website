7c8b74737e4ad2512b3e49a6a7b49ec8440e09fd
Alexander Hu <alexanderhu77@ucla.edu>
2025-10-27 20:59:33 -0700

    dropdown for posterdays, update wording on home

--- components/Header.js
+++ components/Header.js
@@ -24,7 +24,9 @@ class Header extends HTMLElement {
             margin-right: 50px;
             padding: 0;
           }
-          
+          li {
+            position: relative;
+          }
           a {
             font-weight: lighter;
             font-size: 15px;
@@ -38,6 +40,38 @@ class Header extends HTMLElement {
             box-shadow:  12px 12px 2px 1px rgba(0, 0, 255, .2);
           }
 
+          .dropdown-content {
+            display: none;
+            flex-direction: column;
+            position: absolute;
+            top: 100%;
+            left: 0;
+            background-color: #F5F2EE;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px rgba(0,0,0,0.2);
+            z-index: 1;
+            border-radius: 8px;
+          }
+
+          .dropdown-content a {
+            display: block;
+            padding: 10px 15px;
+            margin: 0;
+            color: #000;
+          }
+
+          .dropdown-content a:hover {
+            background-color: rgba(0, 0, 255, 0.1);
+          }
+
+          .dropdown:hover .dropdown-content {
+            display: flex;
+          }
+
+          .dropdown > a {
+            cursor: default;
+          }
+            
           .headerlogo {
             max-height: 60px;
             margin-left: 30px;
@@ -52,7 +86,13 @@ class Header extends HTMLElement {
               <li><a href="index.html">Home</a></li>
               <li><a href="about.html">About</a></li>
               <li><a href="contact.html">Contact</a></li>
-              <li><a href="posterdayarchive.html">Poster Day Archive</a></li>
+              <li class="dropdown">
+                <a>Poster Day Archive</a>
+                <div class="dropdown-content">
+                  <a href="posterday2024.html">2024</a>
+                  <!--  <a href="posterday2025.html">2025</a>  -->
+                </div>
+              </li>
               <li><a href="recent.html">Recent Events</a></li>
               <!-- <li><a href="future_events.html">Future Events</a></li> -->
               <li><a href="https://forms.gle/3kmusUSjUaAQ7LQo6" target="_blank">Stay Updated!</a></li>
